16f959a0772349554dddc46e79cbc9fb63677562
Chakita <chakitapesu@gmail.com>
2025-02-23 17:28:01 -0800

    "fixed relative import issuea"

--- EvaluationPipeline/main.py
+++ EvaluationPipeline/main.py
@@ -1,4 +1,9 @@
 import argparse
+
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from evaluation import evaluate_dataset
 from Utils.json_utils import load_json, save_json
 
